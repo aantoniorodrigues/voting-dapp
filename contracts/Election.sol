aea5088138330c578e4aae35fd57e9387585b1e1
aantoniorodrigues <antoniocvpr@gmail.com>
2021-09-04 12:01:13 +0100

    intermediate commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,28 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-    // Store candidate.
-    // Read candidates' name.
-    string public candidate;
+
+    // Model a candidate using a structure type.
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store candidates using a mapping.
+    mapping(uint => Candidate) public candidates;
+
+    // Variable to store the number of candidates.
+    uint public candidatesCount;
+
     // Constructor.
     constructor() public {
-        candidate = 'Candidate 1';
+        addCandidate('Candidate 1');
+        addCandidate('Candidate 2');
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
